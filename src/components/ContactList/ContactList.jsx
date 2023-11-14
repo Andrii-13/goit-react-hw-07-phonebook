f170998ac8022098c89f09e4b23a596b414c014b
Andrii-13 <128071322+Andrii-13@users.noreply.github.com>
2023-11-14 12:41:10 +0200

    finish

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,28 +1,23 @@
 import { List } from './ContactList.styled';
 import { useSelector, useDispatch } from 'react-redux';
 import { deleteContact } from 'redux/operations';
-import { selectContactItems, selectFilter } from 'redux/selectors';
+import { selectVisibleContacts } from 'redux/selectors';
+
 
 export const ContactList = () => {
-  const contacts = useSelector(selectContactItems);
-  const filterContacts = useSelector(selectFilter);
+  
   const dispatch = useDispatch();
 
   const onDeleteContact = contactId => {
     dispatch(deleteContact(contactId));
   };
 
-
-  const findContact = () => {
-    const filterContact = contacts.filter(({ name }) => {
-      return name.includes(filterContacts);
-    });
-    return filterContact;
-  };
+  const visibleContacts = useSelector(selectVisibleContacts);
+ 
   return (
     <List>
       {[
-        findContact().map(({ id, name, number }) => {
+        visibleContacts.map(({ id, name, number }) => {
           return (
             <li key={id}>
               {name}: {number}
